bea2787c4c43b2a4416ba7dc7907001400ed3972
Vladimir <lamaxur@gmail.com>
2024-07-24 14:11:12 +0400

    init proj

---
+++ LLM_Recognizer/LLM_Recognizer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LLM_Recognizer
+//
+//  Created by Vladimir Stasenko on 24.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
