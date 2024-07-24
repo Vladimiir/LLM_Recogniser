b59c25b4cc65907bf238fa639c9ddbfb5ba2c362
Vladimir <lamaxur@gmail.com>
2024-07-24 14:34:57 +0400

    show camera

--- LLM_Recognizer/LLM_Recognizer/ViewController.swift
+++ LLM_Recognizer/LLM_Recognizer/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    lazy var cameraButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Camera", for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 22)
+        button.addTarget(self, action: #selector(cameraButtonDidTrigger), for: .touchUpInside)
+        return button
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.backgroundColor = .white
+
+        view.addSubview(cameraButton)
+
+        NSLayoutConstraint.activate([
+            cameraButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            cameraButton.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
     }
 
-
+    @objc func cameraButtonDidTrigger() {
+        let cameraVc = UIImagePickerController()
+        cameraVc.sourceType = .camera
+        present(cameraVc, animated: true, completion: nil)
+    }
 }
 
